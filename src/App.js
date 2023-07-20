9a078e70641598e70a2fe07096d880fb35f8e0ab
giorgi <giorgikochuashvili2002@gmail.com>
2023-07-20 12:23:21 +0400

    added redux and changed user to reducer for redux

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentification/authentification.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from './utils/firebase/firebase.utils';
+import { setCurrentUser } from './store/user/user.action';
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
--- src/index.js
+++ src/index.js
@@ -3,22 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import reportWebVitals from './reportWebVitals';
 import { CartProvider } from './contexts/cart.context';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
