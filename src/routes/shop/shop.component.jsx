11239a76b8e2053a11504a0972ecf38fc4c2e617
giorgi <giorgikochuashvili2002@gmail.com>
2023-07-21 05:55:09 +0400

    refactored fetching and selecting logic

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,14 +4,14 @@ import Category from '../category/category.component';
 import './shop.styles.scss';
 import { useEffect } from 'react';
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-import { setCategoriesMap } from '../../store/categories/category.action';
+import { setCategories } from '../../store/categories/category.action';
 import { useDispatch } from 'react-redux';
 const Shop = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      dispatch(setCategoriesMap(categoryMap));
+      const caregoriesArray = await getCategoriesAndDocuments();
+      dispatch(setCategories(caregoriesArray));
     };
     getCategoriesMap();
   }, []);
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,15 +1,15 @@
 import { CATEGIRY_ACTION_TYPES } from './categories.types';
 export const initialState = {
-  categoriesMap: {},
+  categoriesArray: [],
 };
 
 export const categoryReducer = (state = initialState, action = {}) => {
   const { type, payload } = action;
   switch (type) {
-    case CATEGIRY_ACTION_TYPES.SET_CATEGORIES_MAP:
+    case CATEGIRY_ACTION_TYPES.SET_CATEGORIES:
       return {
         ...state,
-        categoriesMap: payload,
+        categoriesArray: payload,
       };
     default:
       return state;
