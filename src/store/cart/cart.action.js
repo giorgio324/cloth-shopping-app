a549ccf210d7286ce3329e97c8d957a6f0a37965
giorgi <giorgikochuashvili2002@gmail.com>
2023-07-22 19:27:41 +0400

    added more selector

--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,5 +1,49 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CART_ACTION_TYPES } from './cart.types';
+export const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
 
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+export const deleteItem = (cartItems, cartItemToRemove) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+};
 export const setIsCartOpen = (bool) =>
   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool);
+
+export const addItemToCart = (cartItems, product) => {
+  const newCartItems = addCartItem(cartItems, product);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
+export const removeItemFromCart = (cartItems, cartItemToremove) => {
+  const newCartItems = removeCartItem(cartItems, cartItemToremove);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
+export const deleteItemFromCart = (cartItems, cartItemToDelete) => {
+  const newCartItems = deleteItem(cartItems, cartItemToDelete);
+  return createAction(CART_ACTION_TYPES.SET_CART_ITEMS, newCartItems);
+};
