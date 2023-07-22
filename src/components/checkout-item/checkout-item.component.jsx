085163b4e94b5efaf8672c8fd6fcdc002080c6d3
giorgi <giorgikochuashvili2002@gmail.com>
2023-07-22 21:08:13 +0400

    finished redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,12 +1,20 @@
 import './checkout-item.styles.scss';
-import { useContext } from 'react';
-import { CartContext } from '../../contexts/cart.context';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCartItems } from '../../store/cart/cart.selector';
+import {
+  addItemToCart,
+  removeItemFromCart,
+  deleteItemFromCart,
+} from '../../store/cart/cart.action';
+
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
-  const { deleteItemFromCart, addItemToCart, removeItemFromCart } =
-    useContext(CartContext);
-  const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => removeItemFromCart(cartItem);
+  const cartItems = useSelector(selectCartItems);
+  const dispatch = useDispatch();
+
+  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+  const removeItemHandler = () =>
+    dispatch(removeItemFromCart(cartItems, cartItem));
   return (
     <div className='checkout-item-container'>
       <div className='image-container'>
@@ -25,7 +33,7 @@ const CheckoutItem = ({ cartItem }) => {
       <span className='price'>{price}</span>
       <div
         className='remove-button'
-        onClick={() => deleteItemFromCart(cartItem)}
+        onClick={() => dispatch(deleteItemFromCart(cartItems, cartItem))}
       >
         &#10005;
       </div>
